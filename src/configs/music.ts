7f6c543cd8f69468e650629f5391a2b63b6d52ee
codeuntillive <codeuntillive@gmail.com>
2026-05-22 11:55:34 +0530

    change song

--- src/configs/music.ts
+++ src/configs/music.ts
@@ -1,10 +1,10 @@
 import type { MusicData } from "~/types";
 
 const music: MusicData = {
-  title: "Sunflower",
-  artist: "Post Malone / Swae Lee",
-  cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
-  audio: "music/sunflower.mp3"
+  title: "Champions Anthems",
+  artist: "B.R Karan Aujila",
+  cover: "/music/champions-anthem.jpg",
+  audio: "music/Champions Anthem.webm"
 };
 
 export default music;
